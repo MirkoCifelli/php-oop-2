03c1f5e5464e14ad5c115eac90e425c2400820c6
MirkoCifelli <ciffo4@hotmail.it>
2024-02-19 17:08:28 +0100

    Margin botton

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Template PHP</title>
+        <title>E-commerce</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
@@ -40,7 +40,7 @@
 
         <main>
             <div class="container">
-                <h2 class="text-center">
+                <h2 class="text-center mb-4">
                     L'e-commerce prodotti per animali
                 </h2>
                 <div class="scatola d-flex">
